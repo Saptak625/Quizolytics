133df89062b1d26d2c39a4a784c62cbd53a4a698
SaptakD625 <66028584+Saptak625@users.noreply.github.com>
2022-10-13 00:48:41 +0000

    Unigrams, Bigrams, Trigrams, and Quadgrams along with Automatic Algorithm

--- forms.py
+++ forms.py
@@ -1,8 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField
+from wtforms import BooleanField, IntegerField, SubmitField
 from wtforms.validators import DataRequired, Length
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 
 class AnalyzeForm(FlaskForm):
   jsonFile = FileField("Upload JSON file from QuizDB: ", validators=[FileRequired(), FileAllowed(['json'], 'JSON only!')])
+  # automatic = BooleanField("Automatic", default="checked")
+  maxResults = IntegerField("Max Results: ", default=15)
   submit = SubmitField('Submit')
--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from nltk.collocations import *
 from nltk.tokenize.toktok import ToktokTokenizer
 from werkzeug.utils import secure_filename
 
-# nltk.download('punkt')
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '7b7e30111ddc1f8a5b1d80934d336798'
@@ -28,41 +27,98 @@ def analyze():
     data = json.loads(form.jsonFile.data.read())
     texts = [data["data"]["tossups"][i]["text"] for i in range(len(data["data"]["tossups"]))]
     
+    MAX_RESULTS = form.maxResults.data
+    NUM_QUESTIONS = len(texts)
+    automatic = True #Will be added later release.
+    
     # Replace separators and punctuation with spaces
     text = re.sub(r'[.!?,:;/\-\s]', ' ', ' '.join(texts))
     # Remove extraneous chars
     text = re.sub(r'[\\|@#“”*$&~%\(\)*\"]', '', text)
-
-    STOPWORDS = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
-    quizbowlKeywords = ['title', 'character', 'points', 'work']
+    text = text.lower()
     
     toktok = ToktokTokenizer()
-
-    def common_unigrams(text, frequency=0.40):
+    STOPWORDS = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
+    quizbowlKeywords = ['title', 'character', 'points', 'work', 'novel', 'poem', 'book', 'name', 'story', 'man', 'one', 'narrator', 'novella', 'author', 'another', 'found', 'comes', 'come', 'called', 'poet', 'speaker', 'like', 'opens', 'includes', 'piece', 'begins', 'use', 'used', 'features', 'played', 'within', 'written', 'composer', 'protagonist']
+    
+    def common_unigrams(text, num=15, otherWords=[]):
       allWords = toktok.tokenize(text)
       allWordDist = nltk.FreqDist(w.lower() for w in allWords)
       #Remove stopwords or quizbowl indicators
-      stopwords = STOPWORDS + quizbowlKeywords
-      allWordExceptStopDist = nltk.FreqDist(w for w in allWords if len(w) > 2 and w.lower() not in stopwords)
-      return [i[0] for i in allWordExceptStopDist.most_common(15)]
+      stopwords = STOPWORDS + quizbowlKeywords + otherWords
+      allWordExceptStopDist = nltk.FreqDist(w for w in allWords if len(w) > 2 and w.lower() not in stopwords and not any([(w in i) for i in otherWords]))
+      return [i[0] for i in allWordExceptStopDist.most_common(num)]
     
-    def common_bigrams(text, frequency=0.28):
+    def common_bigrams(text, frequency=0.40, otherWords=[]):
       bigram_measures = nltk.collocations.BigramAssocMeasures()
       
       # change this to read in your data
       finder = BigramCollocationFinder.from_words(toktok.tokenize(text))
       
       # only bigrams that apper at certain frequency
-      finder.apply_freq_filter(int(len(texts)*frequency))
+      finder.apply_freq_filter(round(len(texts)*frequency))
+      
+      ignored_words = STOPWORDS + quizbowlKeywords
+      finder.apply_word_filter(lambda w: len(w) < 3 or w.lower() in ignored_words or any([(w in i) for i in otherWords]))
+      
+      # return the 20 n-grams with the highest PMI
+      results = finder.nbest(bigram_measures.pmi, 50)
+      return [' '.join(i) for i in results]
+    
+    def common_trigrams(text, frequency=0.40, otherWords=[]):
+      trigram_measures = nltk.collocations.TrigramAssocMeasures()
+      
+      # change this to read in your data
+      finder = TrigramCollocationFinder.from_words(toktok.tokenize(text))
+      
+      # only bigrams that apper at certain frequency
+      finder.apply_freq_filter(round(len(texts)*frequency))
+      
+      ignored_words = STOPWORDS + quizbowlKeywords
+      finder.apply_word_filter(lambda w: len(w) < 3 or w.lower() in ignored_words or any([(w in i) for i in otherWords]))
+      
+      # return the 20 n-grams with the highest PMI
+      results = finder.nbest(trigram_measures.pmi, 50)
+      return [' '.join(i) for i in results]
+    
+    def common_quadgrams(text, frequency=0.40, otherWords=[]):
+      quadgram_measures = nltk.collocations.QuadgramAssocMeasures()
+      
+      # change this to read in your data
+      finder = QuadgramCollocationFinder.from_words(toktok.tokenize(text))
+      
+      # only bigrams that apper at certain frequency
+      finder.apply_freq_filter(round(len(texts)*frequency))
       
       ignored_words = STOPWORDS + quizbowlKeywords
       finder.apply_word_filter(lambda w: len(w) < 3 or w.lower() in ignored_words)
       
       # return the 20 n-grams with the highest PMI
-      results = sorted(finder.nbest(bigram_measures.pmi, 50))
+      results = finder.nbest(quadgram_measures.pmi, 50)
       return [' '.join(i) for i in results]
+    
+    def automatic_solving(func, text, automatic, frequency=None, otherWords=[]):
+      if automatic:
+        startingFrequency = 2/NUM_QUESTIONS
+        results = func(text, frequency=startingFrequency, otherWords=otherWords)
+        while len(results) > MAX_RESULTS:
+          results = func(text, frequency=startingFrequency, otherWords=otherWords)
+          startingFrequency += 0.02
+        print(startingFrequency)
+        return results
+      else:
+        return func(text, frequency=frequency, otherWords=otherWords)
+    
+    quadgrams = automatic_solving(common_quadgrams, text, automatic)
+    trigrams = automatic_solving(common_trigrams, text, automatic, otherWords=quadgrams)
+    bigrams = automatic_solving(common_bigrams, text, automatic, otherWords=quadgrams+trigrams)
+    unigrams = common_unigrams(text, num=MAX_RESULTS, otherWords=quadgrams+trigrams+bigrams)
+    # print(unigrams)
+    # print(bigrams)
+    # print(trigrams)
+    # print(quadgrams)
   
-    data = (common_unigrams(text), common_bigrams(text))
+    data = (unigrams, bigrams, trigrams, quadgrams)
   return render_template('analyze.html', form=form, data=data)
 
-# app.run(host='0.0.0.0', port=81)
+app.run(host='0.0.0.0', port=81)
